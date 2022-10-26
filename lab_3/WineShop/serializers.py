6d175b2d73db4f4e5526173a80aee2fee1898a6a
Shdav <shdav123456@gmail.com>
2022-10-26 15:34:58 +0300

    done

---
+++ lab_3/WineShop/serializers.py
@@ -0,0 +1,34 @@
+from WineShop.models import Wine, Manufacturer, Customer, Category
+from rest_framework import serializers
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Customer
+        # Поля, которые мы сериализуем
+        fields = ["pk", "login", "password", "favourites"]
+
+
+class WineSerializer(serializers.ModelSerializer):
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Wine
+        # Поля, которые мы сериализуем
+        fields = ["pk", "name", "manufacturer", "category", "description", "prise"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Category
+        # Поля, которые мы сериализуем
+        fields = ["pk", "name"]
+
+
+class ManufacturerSerializer(serializers.ModelSerializer):
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Manufacturer
+        # Поля, которые мы сериализуем
+        fields = ["pk", "name"]
--- lab_3/WineShop/views.py
+++ lab_3/WineShop/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from WineShop.serializers import WineSerializer, CategorySerializer, CustomerSerializer, ManufacturerSerializer
+from WineShop.models import Wine, Category, Customer, Manufacturer
 
-# Create your views here.
+
+class WineViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, который позволяет просматривать и редактировать акции компаний
+    """
+    # queryset всех пользователей для фильтрации по дате последнего изменения
+    queryset = Wine.objects.all().order_by('date_modified')
+    serializer_class = WineSerializer  # Сериализатор для модели
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, который позволяет просматривать и редактировать акции компаний
+    """
+    # queryset всех пользователей для фильтрации по дате последнего изменения
+    queryset = Category.objects.all().order_by('date_modified')
+    serializer_class = CategorySerializer  # Сериализатор для модели
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, который позволяет просматривать и редактировать акции компаний
+    """
+    # queryset всех пользователей для фильтрации по дате последнего изменения
+    queryset = Customer.objects.all().order_by('date_modified')
+    serializer_class = CustomerSerializer  # Сериализатор для модели
+
+
+class ManufacturerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, который позволяет просматривать и редактировать акции компаний
+    """
+    # queryset всех пользователей для фильтрации по дате последнего изменения
+    queryset = Manufacturer.objects.all().order_by('date_modified')
+    serializer_class = ManufacturerSerializer  # Сериализатор для модели
